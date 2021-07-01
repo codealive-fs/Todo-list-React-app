804f3bbf6e41522a1e2f8fc9539ae6a3577c0802
codealive-fs <fizi.live@gmail.com>
2021-07-01 11:19:11 +0500

    Update to Todos.js
    Update to Todos.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
+
 
 function App() {
+  const onDelete = () => {
+    console.log("I am ondelete");
+  }
+  let todos = [
+    {
+        sno: 1,
+        title: "Go to the market",
+        disc: "You need to go to the market to get this job done"
+    },
+    {
+        sno: 2,
+        title: "Go to the gym",
+        disc: "You need to go to the gym for exercising"
+    },
+    {
+        sno: 3,
+        title: "Go to the school",
+        disc: "You need to go to the school for studying"
+    }
+]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header title="My Todos List" searchBar={false} />
+    <Todos todos={todos} onDelete={onDelete} />
+    <Footer />
+    </>
   );
 }
 
